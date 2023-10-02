dfdf0357e44c54a2e5ed5c659ed4b75e90583896
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-10-02 20:32:58 +0300

    Added a typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
+// pages
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
+      <Routes>
+        <Route path="/" exact element = {<Home />} />
+
+
+      </Routes>
+
       </BrowserRouter>
       
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,57 @@
+ import { Link } from 'react-router-dom';
+ import React, { useState, useEffect } from 'react';
+ import '../App.css';
+
+export default function Home () {
+    const [text, setText] = useState('');
+  const phrases = ['Web Developer', 'Front-End Developer', 'UI/UX Designer']; // Add your phrases here
+  const typingSpeed = 100; // Speed in milliseconds
+  const delayBetweenPhrases = 1500; // Delay between phrases in milliseconds
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let charIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      if (charIndex < phrases[currentIndex].length) {
+        setText(phrases[currentIndex].substring(0, charIndex + 1));
+        charIndex++;
+      } else {
+        setTimeout(() => {
+          charIndex = 0;
+          currentIndex = (currentIndex + 1) % phrases.length;
+          setText('');
+        }, delayBetweenPhrases);
+      }
+    }, typingSpeed);
+
+    return () => clearInterval(typingInterval);
+  }, []);
+    
+
+    return (
+        <div className="home">
+            <div className="content">
+<h1><em>Sally Mugisha Wanga</em></h1>
+            <h2 className="typewriter">
+                  <span>{text}</span>
+                  <span className="cursor"></span></h2>
+            <ul>
+
+                <li>
+                   <Link to="/">About Me</Link> 
+                </li>
+                <li>
+                    <Link to="/">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Contact Me</Link>
+                    </li>
+            </ul>
+            </div>
+            
+
+            </div>
+    );
+
+}
